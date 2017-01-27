8c7b8ef3944121c8b58f732cc114e64ef01c0109
Shahruk Khan <work@shahruk.com>
2017-01-26 21:09:19 -0500

    Save progress on new Bloomsbury shopping experience

--- script.js
+++ script.js
@@ -1,47 +1,102 @@
-$(function() {
-  var $module = $('.js-bloomsbury-shop');
-  var $landing = $('.js-bloomsbury-shop-landing', $module);
-  var $nav = $('.js-bloomsbury-shop-nav', $module);
-  var $pages = $('.js-bloomsbury-shop-page', $module);
+//@TODO - REMOVE
+var Drupal = {
+  behaviors: {
 
-  $pages.each(function() {
-    var title = $(this).data('title');
-    var key = $(this).data('key');
-    $nav.append('<a class="' + key + '" href="#/' + key + '" data-key="' + key + '"><span>' + title + '</span></a>');
-  });
+  }
+};
 
-  var routeActions = {
-    init: function() {
-      $('.js-bloomsbury-slider.slick-initialized', $module).slick('destroy');
-      $landing.hide();
-      $pages.hide();
-      $pages.filter('[data-key="'+ router.getRoute() +'"]').fadeIn();
-      $nav.find('a').removeClass('active');
-      $nav.find('a[data-key="'+ router.getRoute() +'"]').addClass('active');
+
+(function($) {
+
+  Drupal.behaviors.bloomsburyExperiencev1 = {
+    attach: function(context) {
+      var animationTime = 600;
+      var $module = $('.js-bloomsbury-shop', context);
+      var $landing = $('.js-bloomsbury-shop-landing', $module);
+      var $nav = $('.js-bloomsbury-shop-nav', $module);
+      var $pages = $('.bloomsbury-shop__pages-wrap', $module);
+      var $singlePages = $('.js-bloomsbury-shop-page', $module);
+
+      function updateResponsive() {
+        // $module.height(window.innerHeight - $('#top').outerHeight());
+      }
+
+
+      updateResponsive();
+      $(window).on('resize.bloomsbury', updateResponsive);
+
+      $singlePages.each(function() {
+        var title = $(this).data('title');
+        var key = $(this).data('key');
+        $nav.append('<a class="' + key + '" href="#/' + key + '" data-key="' + key + '"><span>' + title + '</span></a>');
+      });
+
+      var routeActions = {
+        init: function(cb) {
+          $module.removeClass('landing');
+          $landing.fadeOut(animationTime);
+          $pages.fadeOut(animationTime, function() {
+            $singlePages.hide();
+            $singlePages.filter('[data-key="'+ router.getRoute() +'"]').show();
+            $nav.find('a').removeClass('active');
+            $nav.find('a[data-key="'+ router.getRoute() +'"]').addClass('active');
+            setTimeout(function() {
+              $pages.fadeIn(animationTime);
+              if (cb) {
+                cb();
+              }
+            }, 200);
+          });
+        }
+      };
+
+      var routes = {
+        '/story': function() {
+          routeActions.init();
+        },
+        '/scents': function() {
+          routeActions.init();
+          $('.bloomsbury-shop__page--scents .js-bloomsbury-slider.slick-initialized', $module).slick('unslick');
+        },
+        '/inspiration': function() {
+          routeActions.init(function() {
+            setTimeout(function() {
+              $('.bloomsbury-shop__page--inspiration .js-bloomsbury-slider', $module).slick({
+                speed: animationTime
+              });
+            }, 100);
+          });
+        }
+      };
+
+      var router = Router(routes);
+
+      router.init();
+
+      $(document).on('click', '.js-bloomsbury-shop-open', function(event) {
+        event.preventDefault();
+        var index = $(this).parents('.bloomsbury-shop__products-item').index();
+        $('.bloomsbury-shop__products-item:not(:eq('+index+'))', $module).fadeOut(animationTime, function() {
+          $('.bloomsbury-shop__products-item', $module).attr('style', '');
+          // $module.find('.bloomsbury-shop__page--scents .js-bloomsbury-slider').css({opacity: 0}).animate({
+          //   opacity: 1,
+          // }, animationTime, function() {
+            setTimeout(function() {
+              $module.find('.bloomsbury-shop__page--scents .js-bloomsbury-slider:not(.slick-initialized)').slick({
+                speed: animationTime
+              });
+              $module.find('.bloomsbury-shop__page--scents .js-bloomsbury-slider').slick('slickGoTo', index, true);
+            }, 100);
+          // });
+        });
+      });
     }
   };
+})(jQuery);
 
-  var routes = {
-    '/story': function() {
-      routeActions.init();
-    },
-    '/scents': function() {
-      routeActions.init();
-    },
-    '/inspiration': function() {
-      routeActions.init();
-      $('.js-bloomsbury-slider').slick();
-    }
-  };
 
-  var router = Router(routes);
-
-  router.init();
-
-  $('.js-bloomsbury-shop-open').on('click', function(event) {
-    event.preventDefault();
-    var index = $(this).parent().index();
-    $(this).parents('.js-bloomsbury-slider').slick();
-    $(this).parents('.js-bloomsbury-slider').slick('slickGoTo', index);
+(function($) {
+  $(document).ready(function() {
+    Drupal.behaviors.bloomsburyExperiencev1.attach($(document));
   });
-})
+})(jQuery);
