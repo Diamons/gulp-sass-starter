b76ac10f77dd61c62d8b18ed1472603de7abec42
Shahruk Khan <work@shahruk.com>
2017-01-29 17:32:58 -0500

    Save bloomsbury - extra large complete

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ var Drupal = {
       };
 
       function updateResponsive() {
-        // $module.height(window.innerHeight - $('#top').outerHeight());
+        $module.height(window.innerHeight - $('#top').height() + 60);
       }
 
 
@@ -39,9 +39,9 @@ var Drupal = {
 
       var routeActions = {
         init: function(cb) {
-          $landing.stop(true, true).fadeOut(animationTime, function() {
+          $landing.stop(true, true).fadeOut(animationTime).promise().done(function() {
             $module.removeClass('landing');
-            $pages.stop(true, true).fadeOut(animationTime, function() {
+            $pages.stop(true, true).fadeOut(animationTime).promise().done(function() {
               $singlePages.hide();
               $singlePages.filter('[data-key="'+ router.getRoute() +'"]').show();
               $nav.find('a').removeClass('active');
@@ -90,27 +90,32 @@ var Drupal = {
         if (index < 0) {
           index = 0;
         }
-        $('.bloomsbury-shop__products-item:not(:eq('+index+'))', $module).stop(true, true).fadeOut(animationTime, function() {
-          transitionToScent(index);
+        $('.bloomsbury-shop__products-item:not(:eq('+index+'))', $module).stop(true, true).animate({opacity: 0}).promise().done(function() {
+          $('.bloomsbury-shop__products-item:eq('+index+')', $module).stop(true, true).animate({opacity: 0}).promise().done(function() {
+            $('.bloomsbury-shop__products-item', $module).hide().css({opacity: 1});
+            transitionToScent(index);
+          });
         });
       });
 
       function transitionToScent(index) {
           setTimeout(function() {
             var $scentsCarousel = $module.find('.bloomsbury-shop__page--scents .js-bloomsbury-slider:not(.slick-initialized)');
-            $scentsCarousel.slick(slickSettings);
+            $scentsCarousel.slick(slickSettings).hide().fadeIn();
             // Arrow themes
             $scentsCarousel.on('beforeChange', function(event, slick, currentSlide, nextSlide){
               var currentClass = $scentsCarousel.find('.slick-slide:not(.slick-cloned)').eq(currentSlide).data('theme');
               var nextClass = $scentsCarousel.find('.slick-slide:not(.slick-cloned)').eq(nextSlide).data('theme');
               $scentsCarousel.removeClass(currentClass).addClass(nextClass);
             });
-            $('.bloomsbury-shop__products-item', $module).stop(true, true).fadeIn(animationTime, function() {
-              $module.find('.bloomsbury-shop__page--scents .js-bloomsbury-slider').slick('slickGoTo', index, true);
-              $('.bloomsbury-shop__page--scents', $module).stop(true, true).fadeIn(animationTime);
+            $('.bloomsbury-shop__products-item', $module).stop(true, true).fadeIn(animationTime);
+            $('.bloomsbury-shop__page--scents', $module).stop(true, true).fadeIn(animationTime);
+            $module.find('.bloomsbury-shop__page--scents .js-bloomsbury-slider').slick('slickGoTo', index, true);
+            $scentsCarousel.on('destroy', function(event, slick) {
+              $scentsCarousel.attr('class', 'js-bloomsbury-slider');
             });
           // }, 1);
-        });
+        }, 1);
       }
     }
   };
